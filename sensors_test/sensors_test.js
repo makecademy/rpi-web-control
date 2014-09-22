2c3f07f9299880fdbe58ba9165248fe9fed018a5
marcoschwartz <marcolivier.schwartz@gmail.com>
2014-09-22 11:42:57 +0200

    changed address of i2c sensor

--- sensors_test/sensors_test.js
+++ sensors_test/sensors_test.js
@@ -1,18 +1,7 @@
 var sensorLib = require('node-dht-sensor');
 var TSL2561 = require('sensor_tsl2561');
 
-var sense = new TSL2561({
-    'address': '0x29'
-});
-sense.init(function(err, val) {
-  if (!err) {
-    sense.getLux(function(error, val) {
-      if (!error) console.log(val + ' lux');
-    });    
-  }
-});
-
-var sensor = {
+var dht_sensor = {
     initialize: function () {
         return sensorLib.initialize(11, 4);
     },
@@ -21,13 +10,25 @@ var sensor = {
         console.log('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' +
             'humidity: ' + readout.humidity.toFixed(2) + '%');
         setTimeout(function () {
-            sensor.read();
+            dht_sensor.read();
         }, 2000);
     }
 };
 
-if (sensor.initialize()) {
-    sensor.read();
+if (dht_sensor.initialize()) {
+    dht_sensor.read();
 } else {
     console.warn('Failed to initialize sensor');
 }
+
+var sense = new TSL2561({
+    'address': '0x29'
+});
+
+sense.init(function(err, val) {
+  if (!err) {
+    sense.getLux(function(error, val) {
+      if (!error) console.log(val + ' lux');
+    });    
+  }
+});
