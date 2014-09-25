4e25be87ec9667a2b7267b7055f3775e2e3081b9
marcoschwartz <marcolivier.schwartz@gmail.com>
2014-09-25 09:43:43 +0200

    added web app

--- pi_node/pi_node.js
+++ pi_node/pi_node.js
@@ -1,4 +1,16 @@
 var sensorLib = require('node-dht-sensor');
+var express = require('express');
+var app = express();
+var piREST = require('pi-arest')(app);
+
+piREST.set_id('1');
+piREST.set_name('my_RPi');
+
+app.get('/', function(req, res){
+  
+  res.send("Welcome to the home page");
+
+});
 
 var dht_sensor = {
     initialize: function () {
@@ -6,6 +18,10 @@ var dht_sensor = {
     },
     read: function () {
         var readout = sensorLib.read();
+        
+        piREST.variable('temperature',readout.temperature.toFixed(2));
+        piREST.variable('humidity', readout.humidity.toFixed(2));
+        
         console.log('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' +
             'humidity: ' + readout.humidity.toFixed(2) + '%');
         setTimeout(function () {
@@ -18,4 +34,7 @@ if (dht_sensor.initialize()) {
     dht_sensor.read();
 } else {
     console.warn('Failed to initialize sensor');
-}
+
+var server = app.listen(80, function() {
+    console.log('Listening on port %d', server.address().port);
+});
